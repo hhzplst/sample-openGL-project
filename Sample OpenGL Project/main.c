5a37fb512fd0f201bfb51da2f022c93dfbc6fffb
Ivy Chang <ivycwq@icloud.com>
2016-04-04 14:39:03 -0700

    Initial Commit

---
+++ Sample OpenGL Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Sample OpenGL Project
+//
+//  Created by Wanqi Chang on 4/4/16.
+//  Copyright © 2016 Wanqi Chang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
